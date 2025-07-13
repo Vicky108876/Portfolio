eab567e9fa975bbb15ac769f189b773163bf9f09
vicky <vigneshmuthuram2004@gmail.com>
2025-07-13 19:48:12 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 // require("dotenv").config();
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/protfolio");
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDb");
   } catch (error) {
     console.error("Failed to connect ot MongoDb:", error);
