7c5c5beeb9515e7e75ab965ec69cb56678434d3b
vicky <vigneshmuthuram2004@gmail.com>
2025-07-13 23:15:38 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI + "&ssl=true");
+    await mongoose.connect(process.env.MONGODB_URI {
+  ssl: true,
+  sslValidate: false, // ✅ Disables cert validation — necessary for some hosts
+});
     console.log("Connected to MongoDb");
   } catch (error) {
     console.error("Failed to connect ot MongoDb:", error);
